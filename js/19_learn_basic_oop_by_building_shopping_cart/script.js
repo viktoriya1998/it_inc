648180ac9fa73cc63ad76d660c04644ba5cbdd4d
Viktoriya Denisova <viktoriya_d98@mail.ru>
2024-10-28 01:30:42 +0300

    complete steps 53-60, finish

--- js/19_learn_basic_oop_by_building_shopping_cart/script.js
+++ js/19_learn_basic_oop_by_building_shopping_cart/script.js
@@ -138,6 +138,27 @@ class ShoppingCart {
     return this.items.length;
   }
 
+  clearCart() {
+    if (!this.items.length) {
+      alert('Your shopping cart is already empty');
+      return;
+    }
+
+    const isCartCleared = confirm(
+      'Are you sure you want to clear all items from your shopping cart?',
+    );
+
+    if (isCartCleared) {
+      this.items = [];
+      this.total = 0;
+      productsContainer.innerHTML = '';
+      totalNumberOfItems.textContent = 0;
+      cartSubTotal.textContent = 0;
+      cartTaxes.textContent = 0;
+      cartTotal.textContent = 0;
+    }
+  }
+
   calculateTaxes(amount) {
     return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
   }
@@ -160,6 +181,7 @@ const addToCartBtns = document.getElementsByClassName('add-to-cart-btn');
   btn.addEventListener('click', (event) => {
     cart.addItem(Number(event.target.id), products);
     totalNumberOfItems.textContent = cart.getCounts();
+    cart.calculateTotal();
   });
 });
 
@@ -168,3 +190,5 @@ cartBtn.addEventListener('click', () => {
   showHideCartSpan.textContent = isCartShowing ? 'Hide' : 'Show';
   cartContainer.style.display = isCartShowing ? 'block' : 'none';
 });
+
+clearCartBtn.addEventListener('click', cart.clearCart.bind(cart));
