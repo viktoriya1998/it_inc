d85e8a824bc5756f249c05e66fb7e3eb97dc8e05
Viktoriya Denisova <viktoriya_d98@mail.ru>
2024-10-27 23:56:30 +0300

    complete steps 32-40

--- htmlCss/19_learn_basic_oop_by_building_shopping_cart/script.js
+++ htmlCss/19_learn_basic_oop_by_building_shopping_cart/script.js
@@ -108,14 +108,44 @@ class ShoppingCart {
 
   addItem(id, products) {
     const product = products.find((item) => item.id === id);
-
     const { name, price } = product;
     this.items.push(product);
-    const totalCountPerProduct = {};
 
+    const totalCountPerProduct = {};
     this.items.forEach((dessert) => {
       totalCountPerProduct[dessert.id] =
         (totalCountPerProduct[dessert.id] || 0) + 1;
     });
+
+    const currentProductCount = totalCountPerProduct[product.id];
+    const currentProductCountSpan = document.getElementById(
+      `product-count-for-id${id}`,
+    );
+
+    currentProductCount > 1
+      ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
+      : (productsContainer.innerHTML += `
+      <div id="dessert${id}" class="product">
+        <p>
+          <span class="product-count" id="product-count-for-id${id}"></span>${name}
+        </p>
+        <p>${price}</p>
+      </div>
+      `);
   }
 }
+
+const cart = new ShoppingCart();
+const addToCartBtns = document.getElementsByClassName('add-to-cart-btn');
+
+[...addToCartBtns].forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    cart.addItem(Number(event.target.id), products);
+  });
+});
+
+cartBtn.addEventListener('click', () => {
+  isCartShowing = !isCartShowing;
+  showHideCartSpan.textContent = isCartShowing ? 'Hide' : 'Show';
+  cartContainer.style.display = isCartShowing ? 'block' : 'none';
+});
