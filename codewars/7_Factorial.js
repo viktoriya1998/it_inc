25dfc4aac937d8a9b9afcc646615276449a3fefe
Viktoriya Denisova <viktoriya_d98@mail.ru>
2024-10-28 01:57:30 +0300

    complete task

---
+++ codewars/7_Factorial.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a049e253ba33ac5e000212/train/javascript
+
+/* eslint-disable no-unused-vars */
+function factorial(n) {
+  return n > 1 ? 1 : n * factorial(n - 1);
+}
+
+// console.log(factorial(3));
