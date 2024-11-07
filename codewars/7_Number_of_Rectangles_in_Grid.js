5128203a446f1eca5118a40b89391ff89a4a54b3
Viktoriya Denisova <viktoriya_d98@mail.ru>
2024-11-07 16:22:23 +0300

    complete task

---
+++ codewars/7_Number_of_Rectangles_in_Grid.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/556cebcf7c58da564a000045/train/javascript
+
+// eslint-disable-next-line no-unused-vars
+function numberOfRectangles(m, n) {
+  return (m * (m + 1) * n * (n + 1)) / 4;
+}
+
+// console.log(numberOfRectangles(3, 2));
+// console.log(numberOfRectangles(4, 4));
