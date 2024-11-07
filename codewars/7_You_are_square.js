1c10cf4253a2cb36a76c86cbfe7fe69035ce9eb2
Viktoriya Denisova <viktoriya_d98@mail.ru>
2024-11-07 17:34:15 +0300

    complete task

---
+++ codewars/7_You_are_square.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/54c27a33fb7da0db0100040e/solutions/javascript
+
+// eslint-disable-next-line no-unused-vars
+function isSquare(n) {
+  return Number.isInteger(Math.sqrt(n));
+}
