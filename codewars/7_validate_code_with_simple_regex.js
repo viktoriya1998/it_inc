2bb9c18128177792e0533032018dd6b5d464014a
Viktoriya Denisova <viktoriya_d98@mail.ru>
2024-11-08 17:20:44 +0300

    complete task

---
+++ codewars/7_validate_code_with_simple_regex.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/56a25ba95df27b7743000016/train/javascript
+
+// eslint-disable-next-line no-unused-vars
+function validateCode(code) {
+  return /^[123]/.test(code);
+}
+
+// console.log(validateCode(123));
