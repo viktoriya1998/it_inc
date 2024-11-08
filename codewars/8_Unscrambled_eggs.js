fa0420e43bfec1f5ea3eb870d1c383f8dba2742e
Viktoriya Denisova <viktoriya_d98@mail.ru>
2024-11-08 18:36:36 +0300

    complete task

---
+++ codewars/8_Unscrambled_eggs.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55ea5650fe9247a2ea0000a7/train/javascript
+
+// eslint-disable-next-line no-unused-vars
+function unscrambleEggs(word) {
+  return word.replace(/egg/g, '');
+}
+
+// console.log(unscrambleEggs('FeggUNegg KeggATeggA'));
