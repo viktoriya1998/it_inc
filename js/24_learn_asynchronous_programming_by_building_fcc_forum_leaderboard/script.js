381cb13d77cc8ef933e09c2ce91a5c5c1a90be3e
Viktoriya Denisova <viktoriya_d98@mail.ru>
2024-11-09 23:53:45 +0300

    complete steps 1-8

---
+++ js/24_learn_asynchronous_programming_by_building_fcc_forum_leaderboard/script.js
@@ -0,0 +1,15 @@
+const forumLatest = 'https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json';
+const forumTopicUrl = 'https://forum.freecodecamp.org/t/';
+const forumCategoryUrl = 'https://forum.freecodecamp.org/c/';
+const avatarUrl = 'https://sea1.discourse-cdn.com/freecodecamp';
+const postsContainer = document.getElementById('posts-container');
+
+const fetchData = async () => {
+  try {
+    const res = await fetch(forumLatest);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {}
+};
+
+fetchData();
