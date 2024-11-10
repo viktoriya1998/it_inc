0b930c8de49d5453350caf1bdf26daf5c04a54a7
Viktoriya Denisova <viktoriya_d98@mail.ru>
2024-11-10 13:27:09 +0300

    complete steps 9-12

--- js/24_learn_asynchronous_programming_by_building_fcc_forum_leaderboard/script.js
+++ js/24_learn_asynchronous_programming_by_building_fcc_forum_leaderboard/script.js
@@ -8,8 +8,14 @@ const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
     const data = await res.json();
-    console.log(data);
-  } catch (err) {}
+    showLatestPosts(data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 fetchData();
+
+const showLatestPosts = (data) => {
+  const { topic_list, users } = data;
+};
